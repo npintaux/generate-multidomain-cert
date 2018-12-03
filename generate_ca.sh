99824df110b0623481ebcf50d1be17bde4de155f
Nicolas Pintaux <npintaux@pivotal.io>
2018-12-03 14:51:14 +0100

    First revision.

---
+++ generate_ca.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo openssl genrsa -out server.key 2048
+
+sudo openssl req -new -out server.csr -key server.key -config selfsignedca.cnf
+
+sudo openssl x509 -req -days 3650 -in server.csr -signkey server.key -out server.crt -extensions v3_req -extfile selfsignedca.cnf
+
+
